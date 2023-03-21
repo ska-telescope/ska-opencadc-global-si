49c37f8ba9e03f8094de8eac3cb1d05dc1e3c8ef
Franz Kirsten <franz.kirsten@chalmers.se>
2023-03-21 16:16:17 +0000

    first commit of supposedly required files

---
+++ config/ratik/artifact-filter.sql
@@ -0,0 +1,5 @@
+# When org.opencadc.fenwick.artifactSelector=filter is specified, this config file
+# specifying the included Artifacts is required. The single clause in the SQL file
+# MUST begin with the WHERE keyword.
+# ex:
+# WHERE uri LIKE '%SOME CONDITION%'
