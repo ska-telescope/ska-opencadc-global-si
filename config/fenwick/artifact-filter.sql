fa8d81d0cc7118684edfc0b78a151b49d83bb7ec
Franz Kirsten <franz.kirsten@chalmers.se>
2023-04-26 13:54:41 +0000

    added selection criteria for global to fenwick and ratik

--- config/fenwick/artifact-filter.sql
+++ config/fenwick/artifact-filter.sql
@@ -3,3 +3,11 @@
 # MUST begin with the WHERE keyword.
 # ex:
 # WHERE uri LIKE '%SOME CONDITION%'
+where split_part(uri,'/',1) in (
+    'cadc:CGPS',
+    'cadc:VGPS',
+    'cadc:VLASS',
+    'cadc:WALLABY',
+    'casda:RACS',
+    'nrao:VLASS'
+)
