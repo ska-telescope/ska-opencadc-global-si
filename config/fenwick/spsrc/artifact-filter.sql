84af5f711ce992a57eb6d5be4f2b37b398a815fa
Franz Kirsten <franz.kirsten@chalmers.se>
2023-06-13 10:09:23 +0000

    fenwick: first fully tested and working config for multi-site operations (cadc, es-src, se-src, ch-src)

--- config/fenwick/spsrc/artifact-filter.sql
+++ config/fenwick/spsrc/artifact-filter.sql
@@ -1,8 +1,8 @@
-# When org.opencadc.fenwick.artifactSelector=filter is specified, this config file
-# specifying the included Artifacts is required. The single clause in the SQL file
-# MUST begin with the WHERE keyword.
-# ex:
-# WHERE uri LIKE '%SOME CONDITION%'
+-- When org.opencadc.fenwick.artifactSelector=filter is specified, this config file
+-- specifying the included Artifacts is required. The single clause in the SQL file
+-- MUST begin with the WHERE keyword.
+-- ex:
+-- WHERE uri LIKE '%SOME CONDITION%'
 where split_part(uri,'/',1) in (
     'cadc:CGPS',
     'cadc:VGPS',
